e46f3999c67f40081fe32c5448b0566a08093b26
sourav <80613765+sourav-tech-artisan@users.noreply.github.com>
2024-09-06 02:20:11 +0530

    Revert "Initial commit: Set up transaction service application"

--- transactions/main/urls.py
+++
@@ -1,7 +0,0 @@
-from transactions.main.views import TransactionViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'transactions', TransactionViewSet, basename='transactions')
-
-urlpatterns = router.urls
